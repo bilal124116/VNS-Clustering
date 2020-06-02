86caa01e34304be9164c9ca9e95b365e760ba629
bilal124116 <c_khamouj@esi.dz>
2020-06-02 06:43:45 +0100

    Update ScriptSQL.m

--- ScriptSQL.m
+++ ScriptSQL.m
@@ -6,7 +6,7 @@ setdbprefs('NullStringRead', 'null');
 
 %Make connection to database.  Note that the password has been omitted.
 %Using ODBC driver.
-conn = database('MSSQLSERVER', 'sa', 'GMB2015');
+conn = database('MSSQLSERVER', 'sa', 'YourPassword');
 
 %Read data from database.
 curs = exec(conn, ['SELECT 	MesureFEMTO.id'...
